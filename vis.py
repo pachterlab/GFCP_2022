753e8c7d6d4c15fb4a3cbcd39a3dc71aa3a84d99
Tara <tarachari3@gmail.com>
2021-08-25 11:03:41 -0700

    Add comments

--- vis.py
+++ vis.py
@@ -1,5 +1,9 @@
 #For vlm need to save linear/non-linear pca,t-SNE,UMAP,  and true pseudotime/gamma, cluster labels
 
+#git clone https://github.com/pachterlab/GFCP_2021.git
+#import vis 
+#vis.makeEmbeds()....
+
 
 makeEmbeds(vlm,embeds)
 '''
